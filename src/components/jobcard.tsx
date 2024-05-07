52d78aa0fe5c0d50677eaa5455bddbc524efdb34
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-05-07 10:22:10 +0530

    feat: basic job card design

--- src/components/jobcard.tsx
+++ src/components/jobcard.tsx
@@ -1,16 +1,56 @@
 import { Job } from "../store/search-job-slice";
+import './css/jobcard.css'
 
-export function JobCard({
-    job
-}: {
-    job: Job;
-}) {
+export const JobCard = ({ job }: {
+    job: Job
+}) => {
+    const {
+        companyName,
+        jobRole,
+        location,
+        minExp,
+        maxExp,
+        minJdSalary,
+        maxJdSalary,
+        jobDetailsFromCompany,
+        jdLink,
+    } = job;
+
     return (
-        <div>
-            <h1>{job.jobRole}</h1>
-            <h2>{job.companyName}</h2>
-            <h3>{job.location}</h3>
-            <h4>{job.minJdSalary}</h4>
+        <div className="job-card">
+            <div className="job-card-header">
+                <img className="job-logo" src={job.logoUrl} alt={`${companyName} Logo`} />
+                <div className="job-card-title">
+                    <h2 className="company">{companyName}</h2>
+                    <h2 className="role">{jobRole}</h2>
+                    <h3 className="location">{location}</h3>
+                </div>
+            </div>
+            <div className="estimated-salary">
+                <h3>
+                    Estimated Salary:
+                    {minJdSalary ? ` $${minJdSalary}` : ' Not Provided'} -
+                    {maxJdSalary ? ` ${maxJdSalary}` : ' Not Provided'} LPA ✅
+                </h3>
+            </div>
+            <div className="job-card-body">
+                <h3 className="about-company">About Company: </h3>
+                <p className="job-description">{jobDetailsFromCompany}</p>
+            </div>
+            <div className="job-card-footer">
+                <div className="job-card-experience">
+                    <span>
+                        {minExp || maxExp ? (
+                            `${minExp || 'No min exp'} - ${maxExp || 'No max exp'} years exp`
+                        ) : (
+                            'Exp. not provided'
+                        )}
+                    </span>
+                </div>
+                <a href={jdLink} className="job-card-view-more">
+                    View Job
+                </a>
+            </div>
         </div>
     );
-}
+};
